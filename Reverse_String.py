65d867ba2e646b854ade61ca7bd6ff6c1852fa49
Michelle MV <lousclues17@gmail.com>
2026-04-07 12:26:24 -0400

    Completed Reverse String challenge with slicing method

---
+++ Reverse_String.py
@@ -0,0 +1,8 @@
+
+def reverse_string_slice(text):
+    return text[::-1]
+
+# Testing the function
+print(reverse_string_slice("Python")) # Output: nohtyP
+print(reverse_string_slice("Goodbye!")) #Output: !eybdooG
+print(reverse_string_slice("Welcome!")) #Output: !emocleW
